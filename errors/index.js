abcb2c1be13785523cf3c01ef237ce361036e6c7
Samuel Hübner <samu.huebner@gmail.com>
2020-10-28 14:05:05 +0100

    - Adds errors

---
+++ errors/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    BadRequestError: require('./BadRequest.error'),
+    InternalServerError: require('./InternalServer.error'),
+    NotFoundError: require('./NotFound.error'),
+    AuthenticationError: require('./Authenticaiton.error'),
+    UnauthorizedError: require('./Unauthorized.error')
+};
